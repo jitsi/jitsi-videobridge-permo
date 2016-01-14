e8ea89de51e63dfd11a33e0099e8f80da90d92e0
Aaron van Meerten <avanmeerten@atlassian.com>
2016-01-14 15:42:15 -0500

    no need for &

--- jvb-run.sh
+++ jvb-run.sh
@@ -1,4 +1,4 @@
 JVB_VERSION=`dpkg -s jitsi-videobridge | grep Version | awk '{print $2}' | cut -d'-' -f1`
 
 mkdir $JVB_VERSION
-./collect-stats.sh 2&>/dev/null > $JVB_VERSION/data.json
+./collect-stats.sh 2>/dev/null > $JVB_VERSION/data.json
