70d6e33037359014abe463f4baa14738e1519c67
Aaron van Meerten <avanmeerten@atlassian.com>
2016-01-14 22:55:03 -0500

    clear existing data if already present

--- jvb-run.sh
+++ jvb-run.sh
@@ -1,4 +1,8 @@
 JVB_VERSION=`dpkg -s jitsi-videobridge | grep Version | awk '{print $2}' | cut -d'-' -f1`
 
+if [ -d "$JVB_VERSION" ]; then
+    #do nothing here since directory already exists
+    rm -rf $JVB_VERSION
+fi
 mkdir $JVB_VERSION
 ./collect-stats.sh 2>/dev/null > $JVB_VERSION/data.json
