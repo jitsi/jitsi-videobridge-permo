72d176eeaa5826951c824e7ea2b3dbfe8d4bf5ed
Aaron van Meerten <avanmeerten@atlassian.com>
2016-02-14 14:05:04 -0600

    use combined version when calculating stats

--- jvb-run.sh
+++ jvb-run.sh
@@ -1,8 +1,12 @@
-JVB_VERSION=`dpkg -s jitsi-videobridge | grep Version | awk '{print $2}' | cut -d'-' -f1`
+JVB_VERSION_NAME=`dpkg -s jitsi-videobridge | grep Version | awk '{print $2}' | cut -d'-' -f1`
+JICOFO_VERSION_NAME=`dpkg -s jicofo | grep Version | awk '{print $2}' | cut -d'-' -f2`
+JITSI_MEET_VERSION_NAME=`dpkg -s jitsi-meet | grep Version | awk '{print $2}' | cut -d'.' -f3 | cut -d'-' -f1`
 
-if [ -d "$JVB_VERSION" ]; then
+VERSION_STR="${JVB_VERSION_NAME}_${JICOFO_VERSION_NAME}_${JITSI_MEET_VERSION_NAME}"
+
+if [ -d "$VERSION_STR" ]; then
     #do nothing here since directory already exists
-    rm -rf $JVB_VERSION
+    rm -rf $VERSION_STR
 fi
-mkdir $JVB_VERSION
-./collect-stats.sh 2>/dev/null > $JVB_VERSION/data.json
+mkdir $VERSION_STR
+./collect-stats.sh 2>/dev/null > $VERSION_STR/data.json
