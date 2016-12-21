b2092e22b8d83724ef8d872f77e65307c28373d4
Aaron van Meerten <avanmeerten@atlassian.com>
2016-12-21 15:18:55 -0600

    get version of jitsi-meet-web rather than jitsi-meet

--- jvb-run.sh
+++ jvb-run.sh
@@ -2,7 +2,7 @@
 set -x
 JVB_VERSION_NAME=`dpkg -s jitsi-videobridge | grep Version | awk '{print $2}' | cut -d'-' -f1`
 JICOFO_VERSION_NAME=`dpkg -s jicofo | grep Version | awk '{print $2}' | cut -d'-' -f2`
-JITSI_MEET_VERSION_NAME=`dpkg -s jitsi-meet | grep Version | awk '{print $2}' | cut -d'.' -f3 | cut -d'-' -f1`
+JITSI_MEET_VERSION_NAME=`dpkg -s jitsi-meet-web | grep Version | awk '{print $2}' | cut -d'.' -f3 | cut -d'-' -f1`
 PIPELINE_BUILD_NUMBER=$1
 
 VERSION_STR="${PIPELINE_BUILD_NUMBER}_${JVB_VERSION_NAME}_${JICOFO_VERSION_NAME}_${JITSI_MEET_VERSION_NAME}"
